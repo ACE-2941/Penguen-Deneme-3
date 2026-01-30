13269da88b02073e23003ec3404c0d58699f62c0
ACE-2941 <uzmanyy@gmail.com>
2026-01-30 05:07:31 +0300

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ let puan = 0;
 let gameActive = true;
 let gameOverTimer = 0;
 
-// ASSETLER
 const penguinImg = new Image();
 penguinImg.src = "assets/penguin.png";
 
@@ -16,12 +15,12 @@ const bgImg = new Image();
 bgImg.src = "assets/arka-plan.jpg"; 
 
 const buzImg = new Image();
-buzImg.src = "assets/buz.png"; // Yeni sarkıt PNG dosyan
+buzImg.src = "assets/buz.png"; 
 
 const penguin = {
     x: 130, 
     y: 500,
-    w: 200, // Penguen boyutu büyük
+    w: 100, 
     h: 100,
     frameX: 0,
     frameY: 0,
@@ -37,7 +36,6 @@ let obstacles = [];
 let timer = 0;
 let moveDir = 0;
 
-// KONTROLLER
 window.onkeydown = (e) => {
     if (e.key === "ArrowLeft") moveDir = -1;
     if (e.key === "ArrowRight") moveDir = 1;
@@ -76,7 +74,6 @@ function update() {
     penguin.y += penguin.velocityY;
     penguin.velocityY += penguin.gravity;
 
-    // Yer Kontrolü
     if (penguin.y > 500) {
         penguin.y = 500;
         penguin.isJumping = false;
@@ -85,15 +82,14 @@ function update() {
         penguin.maxFrames = 5;
     }
 
-    if (penguin.x < 0) penguin.x = 0;
-    if (penguin.x > canvas.width - penguin.w) penguin.x = canvas.width - penguin.w;
+    // 1. DÜZELTME: Kenar sınırlarını penguenin merkezine göre esnettim
+    if (penguin.x < -20) penguin.x = -20;
+    if (penguin.x > canvas.width - penguin.w + 20) penguin.x = canvas.width - penguin.w + 20;
 
-    // Zorluk Ayarı
     let oyunHizi = (puan < 100) ? 3 : 3 + (puan - 100) * 0.05;
     let uretimSikligi = (puan < 100) ? 80 : 55;
 
     if (++timer > uretimSikligi) {
-        // Sarkıtlar daha uzun ve ince (Genişlik: 50, Boy: 80)
         obstacles.push({ x: Math.random() * (canvas.width - 50), y: -100, w: 50, h: 80 });
         timer = 0;
     }
@@ -104,9 +100,16 @@ function update() {
             obstacles.splice(i, 1);
             puan++;
         }
-        // Hitbox (Sarkıtın ucuna göre çarpışma alanı)
-        if (penguin.x + 30 < o.x + o.w && penguin.x + 70 > o.x && 
-            penguin.y + 20 < o.y + o.h && penguin.y + 85 > o.y) {
+        
+        // 3. DÜZELTME: Hassas Çarpışma (Hitbox)
+        // Penguenin sadece merkezindeki 40px'lik alanı tehlikeli sayıyoruz
+        let pLeft = penguin.x + 35;
+        let pRight = penguin.x + 65;
+        let pTop = penguin.y + 25;
+        let pBottom = penguin.y + 85;
+
+        if (pLeft < o.x + o.w && pRight > o.x && 
+            pTop < o.y + o.h && pBottom > o.y) {
             gameActive = false;
         }
     });
@@ -120,33 +123,25 @@ function update() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // 1. Arka Plan
     if (bgImg.complete && bgImg.naturalWidth > 0) {
         ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
-    } else {
-        ctx.fillStyle = "#87ceeb";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
-    // 2. Penguen
     if (penguinImg.complete && penguinImg.naturalWidth > 0) {
         ctx.drawImage(penguinImg, penguin.frameX * 64, penguin.frameY * 40, 64, 40, penguin.x, penguin.y, penguin.w, penguin.h);
-    } else {
-        ctx.fillStyle = "black";
-        ctx.fillRect(penguin.x, penguin.y, penguin.w, penguin.h);
     }
 
-    // 3. Yeni Sarkıt Buzlar
+    // 2. DÜZELTME: Buzların etrafına siyah kontur (JS Shadow ile)
     obstacles.forEach(o => {
         if (buzImg.complete && buzImg.naturalWidth > 0) {
+            ctx.save();
+            ctx.shadowColor = "black";
+            ctx.shadowBlur = 5; // Siyah çerçeve etkisi
             ctx.drawImage(buzImg, o.x, o.y, o.w, o.h);
-        } else {
-            ctx.fillStyle = "white"; 
-            ctx.fillRect(o.x, o.y, o.w, o.h);
+            ctx.restore();
         }
     });
 
-    // 4. Puan Tablosu
     ctx.fillStyle = "white";
     ctx.font = "bold 26px Arial";
     ctx.shadowColor = "black";
@@ -154,7 +149,6 @@ function draw() {
     ctx.fillText("PUAN: " + puan, 20, 45);
     ctx.shadowBlur = 0;
 
-    // 5. Bitiş Ekranı
     if (!gameActive) {
         ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -165,7 +159,7 @@ function draw() {
         ctx.fillStyle = "white";
         ctx.font = "20px Arial";
         ctx.fillText("Puan: " + puan, canvas.width / 2, canvas.height / 2 + 50);
-        ctx.fillText("EKRANA BASIVER", canvas.width / 2, canvas.height / 2 + 90);
+        ctx.fillText("TEKRAR İÇİN TIKLA", canvas.width / 2, canvas.height / 2 + 90);
         ctx.textAlign = "left";
     }
 }
@@ -176,7 +170,6 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
 }
 
-// Fare/Tıkla yeniden başlatma
 canvas.addEventListener("mousedown", () => {
     if (!gameActive) resetGame();
 });
